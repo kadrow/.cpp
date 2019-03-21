675b56db6d1f6183266f54875d24e902f909b6c1
kadrow <48800491+kadrow@users.noreply.github.com>
2019-03-21 15:59:55 +0000

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int i;
+    cout << "Hello, what number do you want to add 1 to: ";
+    cin >> i;
+    cout << i << " + 1 is equal to " << i+1 << ".\n";
+    return 0;
+}
