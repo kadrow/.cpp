5b970c2b3639b4a0494c56aaaf82106bf1fd9282
kadrow <48800491+kadrow@users.noreply.github.com>
2019-03-21 16:02:12 +0000

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+std::cout <<"So I am just checking\n if I can drop lines\n like this\n\n\n";
+std::cout << "sweet! I can!!\n";
+    return 0;
+}
